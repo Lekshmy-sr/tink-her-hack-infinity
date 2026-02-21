eb69aeb2e9c43a2e893a0baed77916f5335fb6a5
Lekshmy-sr <lekshmys33@gmail.com>
2026-02-21 11:33:00 +0530

    render

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask, request, jsonify, session, render_template
 from flask_cors import CORS
 from models import db, User, Bill
 from werkzeug.security import generate_password_hash, check_password_hash
+# web: gunicorn app:app
 import os
-import process
 from datetime import timedelta
 
 app = Flask(__name__)
