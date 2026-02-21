9ca2886da3788805598f5c7ca034ffd7d26e8d72
Lekshmy-sr <lekshmys33@gmail.com>
2026-02-21 10:45:23 +0530

    update

--- app.py
+++ app.py
@@ -206,7 +206,7 @@ def serve_upload(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=process.env.PORT or s5000)
+    app.run(debug=True, port=process.env.PORT or 5000)
 
 @app.route('/')
 def home():
